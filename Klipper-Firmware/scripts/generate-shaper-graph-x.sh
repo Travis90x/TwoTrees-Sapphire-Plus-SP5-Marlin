54a90d6b36ad2445487ccc6faed3427f32982005
Travis90x <travis_90@hotmail.it>
2022-09-02 15:29:09 +0200

    Create generate-shaper-graph-x.sh

---
+++ Klipper-Firmware/scripts/generate-shaper-graph-x.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NEWX=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
+DATE=$(date +'%Y-%m-%d-%H%M%S')
+if [ ! -d "/home/pi/klipper_config/input_shaper" ]
+then
+    mkdir /home/pi/klipper_config/input_shaper
+    chown pi:pi /home/pi/klipper_config/input_shaper
+fi
+
+#~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_x_$DATE.png
+~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_x.png
+
+mv /tmp/resonances_x_* /home/pi/klipper_config/input_shaper/
