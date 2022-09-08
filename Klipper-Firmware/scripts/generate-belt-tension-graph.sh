c327c3958f6234f60e2c83d71004a1c13ec276e6
Travis90x <travis_90@hotmail.it>
2022-09-08 21:28:24 +0200

    Update generate-belt-tension-graph.sh

--- Klipper-Firmware/scripts/generate-belt-tension-graph.sh
+++ Klipper-Firmware/scripts/generate-belt-tension-graph.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-sleep 10
+set -x #echo on
+# set -e -u -o pipefail
+
+# sleep 3
 ## Time to write the files of the tests
- 
- FILENAME=$(ls -Art /tmp/raw_data_axis_*_belt-tension-*.csv | tail -n 1)
-date=$(date +'%Y-%m-%d-%H%M%S')
+
+# Take last files
+FILE1=$(ls -Art /tmp/raw_data_axis=1.000,*.csv | tail -n 1)
+FILE2=$(ls -Art /tmp/raw_data_axis=1.000,-1*.csv | tail -n 1)
+
+rm /tmp/File_raw_data*.csv
+cp "$FILE1" /tmp/File_raw_data1.csv
+cp "$FILE2" /tmp/File_raw_data2.csv
+
+ALLCSV=/tmp/raw_data_axis*.csv
+
+DATE=$(date +'%Y-%m-%d-%H%M%S')
  
 OUTDIR=/home/pi/klipper_config/input_shaper
  if [ ! -d "${OUTDIR}" ]; then
@@ -13,11 +25,12 @@ OUTDIR=/home/pi/klipper_config/input_shaper
 fi
 
 
-## File renamed with date
-## ~/klipper/scripts/graph_accelerometer.py -c $FILENAME -o "${OUTDIR}/input_shaper/resonances_$DATE.png"
+## File renamed with date from last two CSV files
+~/klipper/scripts/graph_accelerometer.py -c /tmp/File_raw_data*.csv -o "${OUTDIR}/resonances_$DATE.png"
 
-## Keep the last one png
-~/klipper/scripts/graph_accelerometer.py -c $FILENAME -o ${OUTDIR}/input_shaper/resonances.png
+## Replace old png
+cp "${OUTDIR}/resonances_$DATE.png" "${OUTDIR}/0_resonances.png"
 
 ## Clean TMP folder and let you see all files from klipper
-mv $FILENAME $OUTDIR
+rm /tmp/File_raw_data*.csv
+mv $ALLCSV "$OUTDIR"
