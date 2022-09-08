03da7c4e3871c6651d06657730461e04c6ae348e
Travis90x <travis_90@hotmail.it>
2022-09-08 18:24:57 +0200

    Update generate-belt-tension-graph.sh

--- Klipper-Firmware/scripts/generate-belt-tension-graph.sh
+++ Klipper-Firmware/scripts/generate-belt-tension-graph.sh
@@ -2,23 +2,22 @@
 sleep 10
 ## Time to write the files of the tests
  
+ FILENAME=$(ls -Art /tmp/raw_data_axis_*_belt-tension-*.csv | tail -n 1)
+date=$(date +'%Y-%m-%d-%H%M%S')
  
-NEWX=$(ls -Art /tmp/raw_data_axis_*.csv | tail -n 1)
-DATE=$(date +'%Y-%m-%d-%H%M%S')
- 
-outdir=/home/pi/klipper_config/input_shaper
- if [ ! -d "${outdir}" ]; then
-    mkdir "${outdir}"
+OUTDIR=/home/pi/klipper_config/input_shaper
+ if [ ! -d "${OUTDIR}" ]; then
+    mkdir "${OUTDIR}"
     ## Edit below with your username
-    chown pi:pi "${outdir}" 
+    chown pi:pi "${OUTDIR}" 
 fi
 
 
-## File renamed with Date
-## ~/klipper/scripts/graph_accelerometer.py -c $NEWX -o /home/pi/klipper_config/input_shaper/resonances.png
+## File renamed with date
+## ~/klipper/scripts/graph_accelerometer.py -c $FILENAME -o "${OUTDIR}/input_shaper/resonances_$DATE.png"
 
 ## Keep the last one png
-~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /home/pi/klipper_config/input_shaper/resonances.png
+~/klipper/scripts/graph_accelerometer.py -c $FILENAME -o ${OUTDIR}/input_shaper/resonances.png
 
 ## Clean TMP folder and let you see all files from klipper
-mv /tmp/raw_data_axis* "${outdir}"
+mv $FILENAME $OUTDIR
