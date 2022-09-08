1c9234f86d6618aab53b5b95c0f74b80c7d5776f
Travis90x <travis_90@hotmail.it>
2022-09-08 22:24:58 +0200

    Update generate-belt-tension-graph.sh

--- Klipper-Firmware/scripts/generate-belt-tension-graph.sh
+++ Klipper-Firmware/scripts/generate-belt-tension-graph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x #echo on
+## set -x #echo on # don't activate if use script in klipper
 # set -e -u -o pipefail
 
 # sleep 3
@@ -9,7 +9,7 @@ set -x #echo on
 FILE1=$(ls -Art /tmp/raw_data_axis=1.000,*.csv | tail -n 1)
 FILE2=$(ls -Art /tmp/raw_data_axis=1.000,-1*.csv | tail -n 1)
 
-rm /tmp/File_raw_data*.csv
+rm /tmp/File_raw_data*.csv > /dev/null 2>&1
 cp "$FILE1" /tmp/File_raw_data1.csv
 cp "$FILE2" /tmp/File_raw_data2.csv
 
