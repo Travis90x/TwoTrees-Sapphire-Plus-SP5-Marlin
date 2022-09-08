baca347f5253b7675e3e35f309789c60a00ff620
Travis90x <travis_90@hotmail.it>
2022-09-08 18:12:19 +0200

    Update generate-shaper-graph-y.sh

--- Klipper-Firmware/scripts/generate-shaper-graph-y.sh
+++ Klipper-Firmware/scripts/generate-shaper-graph-y.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+sleep 3
+## Time to write the files of the tests
 
-NEWX=$(ls -Art /tmp/resonances_y_*.csv | tail -n 1)
+NEWX=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
 DATE=$(date +'%Y-%m-%d-%H%M%S')
-if [ ! -d "/home/pi/klipper_config/input_shaper" ]
-then
-    mkdir /home/pi/klipper_config/input_shaper
-    chown pi:pi /home/pi/klipper_config/input_shaper
+ 
+outdir=/home/pi/klipper_config/input_shaper
+ if [ ! -d "${outdir}" ]; then
+    mkdir "${outdir}"
+    ## Edit below with your username
+    chown pi:pi "${outdir}" 
 fi
 
-#~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_y_$DATE.png
+## File renamed with Date
+## ~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_y_$DATE.png
+
+## Keep the last one png
 ~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_y.png
 
+## Clean TMP folder and let you see all files from klipper
 mv /tmp/resonances_y_* /home/pi/klipper_config/input_shaper/
