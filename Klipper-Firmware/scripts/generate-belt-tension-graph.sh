d4f7b9702af1cb9dc2665101d331ce94d60c53c4
Travis90x <travis_90@hotmail.it>
2022-09-08 18:15:08 +0200

    Update generate-belt-tension-graph.sh

--- Klipper-Firmware/scripts/generate-belt-tension-graph.sh
+++ Klipper-Firmware/scripts/generate-belt-tension-graph.sh
@@ -2,14 +2,22 @@
 sleep 10
 ## Time to write the files of the tests
  
-outdir=/home/pi/klipper_config/input_shaper
  
-if [ ! -d "${outdir}" ]; then
+NEWX=$(ls -Art /tmp/raw_data_axis_*.csv | tail -n 1)
+DATE=$(date +'%Y-%m-%d-%H%M%S')
+ 
+outdir=/home/pi/klipper_config/input_shaper
+ if [ ! -d "${outdir}" ]; then
     mkdir "${outdir}"
     ## Edit below with your username
     chown pi:pi "${outdir}" 
 fi
 
+
+## File renamed with Date
+## ~/klipper/scripts/graph_accelerometer.py -c $NEWX -o /home/pi/klipper_config/input_shaper/resonances.png
+
+## Keep the last one png
 ~/klipper/scripts/graph_accelerometer.py -c /tmp/raw_data_axis*.csv -o /home/pi/klipper_config/input_shaper/resonances.png
 
 ## Clean TMP folder and let you see all files from klipper
