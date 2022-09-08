a237e873816d56781a517e566d4a8539a70324d8
Travis90x <travis_90@hotmail.it>
2022-09-08 21:56:25 +0200

    Update generate-shaper-graph-x.sh

--- Klipper-Firmware/scripts/generate-shaper-graph-x.sh
+++ Klipper-Firmware/scripts/generate-shaper-graph-x.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-sleep 3
-## Time to write the files of the tests
+set -x #echo on
+set -u  pipefail
 
-NEWX=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
+# Take last files
+FILENAME=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
+
+ALLCSV=/tmp/resonances_x_*.csv
 
 DATE=$(date +'%Y-%m-%d-%H%M%S')
- 
-outdir=/home/pi/klipper_config/input_shaper
- 
-if [ ! -d "${outdir}" ]; then
-    mkdir "${outdir}"
+
+OUTDIR=/home/pi/klipper_config/input_shaper
+
+ if [ ! -d "${OUTDIR}" ]; then
+    mkdir "${OUTDIR}"
     ## Edit below with your username
-    chown pi:pi "${outdir}" 
+    chown pi:pi "${OUTDIR}" 
 fi
 
-## File renamed with Date
-## ~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_x_$DATE.png
 
-## Keep the last one png
-~/klipper/scripts/calibrate_shaper.py $NEWX -o /home/pi/klipper_config/input_shaper/resonances_x.png
+## File renamed with date from last two CSV files
+~/klipper/scripts/calibrate_shaper.py $FILENAME -o "${OUTDIR}/shaper_calibrate_x_$DATE.png"
 
-## Clean TMP folder and let you see all files from klipper
-mv /tmp/resonances_x_* /home/pi/klipper_config/input_shaper/
+## Replace old png
+cp "${OUTDIR}/shaper_calibrate_x_$DATE.png" "${OUTDIR}/1_shaper_calibrate_x.png"
+
+mv /tmp/resonances_x_*.csv /home/pi/klipper_config/input_shaper/
+
+mv $ALLCSV "$OUTDIR"
