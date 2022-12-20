46dfdb45281fb75d9920647b058ba014f3b3b637
Travis90x <travis_90@hotmail.it>
2022-12-20 23:05:22 +0100

    Update generate-shaper-graph-x.sh

--- Klipper-Firmware/scripts/generate-shaper-graph-x.sh
+++ Klipper-Firmware/scripts/generate-shaper-graph-x.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ## set -x #echo on # don't activate if use script in klipper
 # set -u  pipefail
-# INPUT SHAPING X
+# INPUT SHAPING Y
 
 # Take last files
-FILENAME=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
+FILENAME=$(ls -Art /tmp/resonances_y_*.csv | tail -n 1)
 
-ALLCSV=/tmp/resonances_x_*.csv
+ALLCSV=/tmp/resonances_y_*.csv
 
 DATE=$(date +'%Y-%m-%d-%H%M%S')
 
@@ -19,11 +19,11 @@ chown $USER:$USER ~/klipper/scripts/
 
 
 ## File renamed with date from last two CSV files
-~/klipper/scripts/calibrate_shaper.py $FILENAME -o "${OUTDIR}/shaper_calibrate_x_$DATE.png"
+~/klipper/scripts/calibrate_shaper.py $FILENAME -o "${OUTDIR}/shaper_calibrate_y_$DATE.png"
 
 ## Replace old png
-cp "${OUTDIR}/shaper_calibrate_x_$DATE.png" "${OUTDIR}/1_shaper_calibrate_x.png"
+cp "${OUTDIR}/shaper_calibrate_y_$DATE.png" "${OUTDIR}/2_shaper_calibrate_y.png"
 
-mv /tmp/resonances_x_*.csv "${OUTDIR}/" > /dev/null 2>&1
+mv /tmp/resonances_y_*.csv "${OUTDIR}/" > /dev/null 2>&1
 
 mv $ALLCSV "${OUTDIR}/" > /dev/null 2>&1
