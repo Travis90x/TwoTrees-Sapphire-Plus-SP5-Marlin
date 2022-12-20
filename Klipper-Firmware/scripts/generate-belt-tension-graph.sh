6920f76ada4debd10f8f51a503c4b5e73f7f942d
Travis90x <travis_90@hotmail.it>
2022-12-20 21:59:35 +0100

    Update generate-belt-tension-graph.sh

--- Klipper-Firmware/scripts/generate-belt-tension-graph.sh
+++ Klipper-Firmware/scripts/generate-belt-tension-graph.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## set -x #echo on # don't activate if use script in klipper
 # set -e -u -o pipefail
+# CINGHIE SINGOLE
 
 # sleep 3
 ## Time to write the files of the tests
@@ -17,20 +18,19 @@ ALLCSV=/tmp/raw_data_axis*.csv
 
 DATE=$(date +'%Y-%m-%d-%H%M%S')
  
-OUTDIR=~/klipper_config/input_shaper
+OUTDIR=~/printer_data/config/input_shaper/
  if [ ! -d "${OUTDIR}" ]; then
     mkdir "${OUTDIR}"
-    ## Edit below with your username
     chown $USER:$USER "${OUTDIR}" 
 fi
 chown $USER:$USER ~/klipper/scripts/
 
 
 ## File renamed with date from last two CSV files
-~/klipper/scripts/graph_accelerometer.py -c /tmp/File_raw_data*.csv -o "${OUTDIR}/resonances_$DATE.png"
+~/klipper/scripts/graph_accelerometer.py -c /tmp/File_raw_data*.csv -o "${OUTDIR}/belts_$DATE.png"
 
 ## Replace old png
-cp "${OUTDIR}/resonances_$DATE.png" "${OUTDIR}/0_resonances.png"
+cp "${OUTDIR}/resonances_$DATE.png" "${OUTDIR}/0_belts.png"
 
 ## Clean TMP folder and let you see all files from klipper
 rm /tmp/File_raw_data*.csv > /dev/null 2>&1
