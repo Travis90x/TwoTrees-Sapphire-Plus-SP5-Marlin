2aa60c45b737994e635256a85aec6431d23fd44b
Travis90x <travis_90@hotmail.it>
2023-01-23 18:34:46 +0100

    Update Configuration_adv.h

--- Marlin-Firmware/Configuration_adv.h
+++ Marlin-Firmware/Configuration_adv.h
@@ -434,7 +434,11 @@
 #define AUTOTEMP
 #if ENABLED(AUTOTEMP)
   #define AUTOTEMP_OLDWEIGHT    0.98  // Factor used to weight previous readings (0.0 < value < 1.0)
-  // Turn on AUTOTEMP on M104/M109 by default using proportions set here
+  #define AUTOTEMP_MIN          210  // Need from Marlin 2.1.3 bugfix
+  #define AUTOTEMP_MAX          250
+  #define AUTOTEMP_FACTOR       0.1f
+
+// Turn on AUTOTEMP on M104/M109 by default using proportions set here
   //#define AUTOTEMP_PROPORTIONAL
   #if ENABLED(AUTOTEMP_PROPORTIONAL)
     #define AUTOTEMP_MIN_P      0 // (°C) Added to the target temperature
