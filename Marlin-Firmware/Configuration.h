325639480ab4fdf6bbbe65d9286af98906349602
Travis90x <travis_90@hotmail.it>
2023-01-23 19:16:42 +0100

    Update Configuration.h

--- Marlin-Firmware/Configuration.h
+++ Marlin-Firmware/Configuration.h
@@ -1479,7 +1479,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 100 }  // 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 810 }  // 
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
