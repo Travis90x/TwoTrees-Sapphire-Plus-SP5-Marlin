71b0e8e1938dcab7ad3cfb221e1d9ef6f7f274a4
Travis90x <travis_90@hotmail.it>
2023-01-23 22:07:30 +0100

    Update Configuration.h

--- Marlin-Firmware/Configuration.h
+++ Marlin-Firmware/Configuration.h
@@ -2,7 +2,7 @@
  * Marlin build for TwoTrees Sapphire Plus with stock hardware
  * Every changed settings could be find by searching for
  * 
- * Last changes 2023-01-23 18:30
+ * Last changes 2023-01-23 22:00
  * Giuseppe Di Resta & Travis90x
  * Marlin 3D Printer Firmware
  * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
